b5d377f3f1e08195baf1f650b4127fca9af3ff38
Kengo Wada <louiskengo3@gmail.com>
2020-02-24 17:47:23 +0300

    Added auth class

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,54 +3,56 @@ import config from './config';
 
 const app = Firebase.initializeApp(config);
 
-function register(email, password) {
-  app
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then((resp) => {
-      return resp;
-    })
-    .catch((error) => {
-      return error;
-    });
+class Auth {
+  static register(email, password) {
+    app
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((resp) => {
+        return resp;
+      })
+      .catch((error) => {
+        return error;
+      });
+  }
+
+  static logIn(email, password) {
+    app
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((resp) => {
+        return resp;
+      })
+      .catch((error) => {
+        return error;
+      });
+  }
+
+  static logInWithGoogle() {
+    const google = new auth.GoogleAuthProvider();
+
+    app
+      .auth()
+      .signInWithPopup(google)
+      .then((resp) => {
+        return resp;
+      })
+      .catch((error) => {
+        return error;
+      });
+  }
+
+  static getToken() {
+    app
+      .auth()
+      .currentUser.getIdToken(false)
+      .then((token) => {
+        return token;
+      })
+      .catch((error) => {
+        return error;
+      });
+  }
 }
 
-function login(email, password) {
-  app
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then((resp) => {
-      return resp;
-    })
-    .catch((error) => {
-      return error;
-    });
-}
-
-function loginWithGoogle() {
-  const google = new auth.GoogleAuthProvider();
-
-  app
-    .auth()
-    .signInWithPopup(google)
-    .then((resp) => {
-      return resp;
-    })
-    .catch((error) => {
-      return error;
-    });
-}
-
-function getToken() {
-  app
-    .auth()
-    .currentUser.getIdToken(false)
-    .then((token) => {
-      return token;
-    })
-    .catch((error) => {
-      return error;
-    });
-}
-
-module.exports = { getToken, login, loginWithGoogle, register };
+module.exports = { Auth };
