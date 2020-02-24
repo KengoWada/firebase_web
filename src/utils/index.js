18b32a56a47f5bf70b594d200b2d011d834a7eda
Kengo Wada <louiskengo3@gmail.com>
2020-02-24 19:50:06 +0300

    Added Posts class

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,6 +4,16 @@ import config from './config';
 const app = Firebase.initializeApp(config);
 
 class Auth {
+  /**
+   * Register a user with Firebase.
+   *
+   * Register a user with Firebase using their email and password.
+   *
+   * @param {String} email     The users email address.
+   * @param {String} password  The users password.
+   *
+   * @returns {Object} Returns the response from the Firebase for both an error and success.
+   */
   static register(email, password) {
     app
       .auth()
@@ -16,6 +26,14 @@ class Auth {
       });
   }
 
+  /**
+   * Log in a user using their email and password.
+   *
+   * @param {String} email     The users email address.
+   * @param {String} password  The users password.
+   *
+   * @returns {Object} Return the response from the Firebase for both an error and success.
+   */
   static logIn(email, password) {
     app
       .auth()
@@ -28,6 +46,11 @@ class Auth {
       });
   }
 
+  /**
+   * Log in user with their Google account.
+   *
+   * @returns {Object} Return the response from the Firebase for both an error and success.
+   */
   static logInWithGoogle() {
     const google = new auth.GoogleAuthProvider();
 
@@ -42,6 +65,11 @@ class Auth {
       });
   }
 
+  /**
+   * Get the users token for authentication.
+   *
+   * @returns {Object} Return the response from the Firebase for both an error and success.
+   */
   static getToken() {
     app
       .auth()
@@ -55,4 +83,45 @@ class Auth {
   }
 }
 
-module.exports = { Auth };
+class Posts {
+  constructor(name, post) {
+    this.name = name;
+    this.post = post;
+    this.postsCollection = app.firestore().collection('Posts');
+  }
+
+  create() {
+    const postData = {
+      name: this.name,
+      post: this.post,
+      uid: app.auth().currentUser.uid,
+      createAt: new Date().toLocaleString(),
+    };
+
+    this.postsCollection
+      .add(postData)
+      .then(resp => {
+        return resp;
+      })
+      .catch(error => {
+        return error;
+      });
+  }
+
+  static getPosts() {
+    this.postsCollection
+      .get()
+      .then(querySnapshot => {
+        let posts = [];
+        querySnapshot.forEach(doc => {
+          posts.push(doc.data());
+        });
+        return posts;
+      })
+      .catch(error => {
+        return error;
+      });
+  }
+}
+
+module.exports = { Auth, Posts };
